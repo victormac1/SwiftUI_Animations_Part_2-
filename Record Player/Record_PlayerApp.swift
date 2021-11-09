291b23b779d1090d42d45fe55b4a7c7d2a099da2
victormac1 <victorcognito@yahoo.com>
2021-11-09 13:11:32 +0100

    first commit

---
+++ Record Player/Record_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Record_PlayerApp.swift
+//  Record Player
+//
+//  Created by Victors Code on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Record_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
